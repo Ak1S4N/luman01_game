21d68ff6ae7793249c67aa5a64e8448158283f97
Ak1S4N <107114197+Ak1S4N@users.noreply.github.com>
2024-10-23 14:50:59 +0800

    10/23

--- src/main/scripts/managers/intman/map1_intman.gd
+++
@@ -1,9 +0,0 @@
-extends Managers
-
-@export var lumber_mill: TextureButton
-@export var slave_quarters: TextureButton
-@export var fishing_hut: TextureButton
-@export var refugee_quarters: TextureButton
-@export var farm: TextureButton
-@export var blacksmith: TextureButton
-@export var hunting_lodge: TextureButton
